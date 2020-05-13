ee68d4f2d109b3223aa1ed4ffaa6ab99fd83ccdd
apple <apple@Macs-iMac.local>
2020-05-13 17:30:41 +0800

    change

--- src/config/development.ts
+++ src/config/development.ts
@@ -1,9 +1,9 @@
 export default {
   // 端口
-  port: parseInt(process.env.PORT, 10) || 3306,
+  port: parseInt(process.env.PORT, 10) || 3000,
   // 是否开启swagger
   enableSwagger: true,
-  // 数据库配置
+  // MySQL配置
   DATABASE_CONFIG: {
     type: 'mysql',
     host: 'localhost',
@@ -16,5 +16,14 @@ export default {
     entities: ['src/**/*.entity{.ts,.js}'],
     synchronize: true,
     logging: true,
+  },
+  // Redis配置
+  REDIS_CONFIG: {
+    host: '127.0.0.1',
+    port: 6379,
+    keyPrefix: '',
+    family: 4,
+    password: '',
+    db: 0,
   },
 };
